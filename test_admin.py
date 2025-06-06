407affdc32054d2a87e4d96707b1e69e42be206c
miguel-fons <miguel_fons@outlook.com>
2025-06-06 10:22:01 -0600

    Actualiza .gitignore para excluir archivos de prueba

--- test_admin.py
+++
@@ -1,6 +0,0 @@
-from admin import create_user_admin_view, get_all_users, export_metrics_to_csv, delete_user_by_admin
-
-print(create_user_admin_view("empleadoPrueba", "claveABC"))
-print(get_all_users())
-print(export_metrics_to_csv("data/reporte.csv"))
-print(delete_user_by_admin(1))  # ID del usuario a eliminar
